539c83a23532ec00e5e8a639e74094b3e38c340c
thrkrif <103479629+thrkrif@users.noreply.github.com>
2022-09-21 17:45:39 +0900

    type
    study

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println(" world!");
+        int level;
+        level = 10;
+        System.out.println(level);
     }
 }
