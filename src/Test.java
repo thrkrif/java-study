cf21590c1fae2578c2f0811d75f6f141cf4af3d0
thrkrif <103479629+thrkrif@users.noreply.github.com>
2022-09-28 23:22:47 +0900

    Test
    study

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-       int i = 10;
-       double j = 2.0;
-       int k = i+(int)j;
-        System.out.println(k);
+       char i = '글';
+       char c2 = '\uAE00';
+        System.out.println(c2);
     }
 }
