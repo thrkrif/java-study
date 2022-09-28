46e4dd5aabac8124ad5c96cc3aa629daffdb1eae
thrkrif <103479629+thrkrif@users.noreply.github.com>
2022-09-28 22:17:20 +0900

    Test
    study

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,8 @@
 public class Test {
     public static void main(String[] args) {
-       byte bnum = 10;
-       int inum = bnum;
-
-        System.out.println(bnum);
-        System.out.println(inum);
+       int i = 10;
+       double j = 2.0;
+       int k = i+(int)j;
+        System.out.println(k);
     }
 }
