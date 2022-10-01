14b73cd594605a4a645d10ef58b0ec67c59cfe4e
thrkrif <103479629+thrkrif@users.noreply.github.com>
2022-10-01 12:39:37 +0900

    Test
    study

--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,9 @@
+import java.util.Arrays;
+
 public class Test {
     public static void main(String[] args) {
-       int x=10;
-       int y=20;
-       int tmp;
-        System.out.println("x="+x);
-        System.out.println("y="+y);
-
-       tmp = x;
-       x = y;
-       y = tmp;
-
-        System.out.println(tmp);
-        System.out.println(x);
-        System.out.println(y);
+        String ur1 = "www.codechobo.com";
+                double d = 1.23456789;
+        System.out.printf("%14.10f", d);
     }
 }
