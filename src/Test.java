2227d938c7ad95aa5b4b32f632bdbb81cbdb0114
thrkrif <103479629+thrkrif@users.noreply.github.com>
2022-10-08 17:20:42 +0900

    Java
    study

--- src/Test.java
+++ src/Test.java
@@ -3,9 +3,6 @@ import java.util.Scanner;
 public class Test {
 
     public static void main(String[] args) {
-        String str = "3";
-
-        System.out.println(str.charAt(0)-'0');
-        System.out.println('3'-'0'+1);
+        System.out.println(true + null);
     }
 }
