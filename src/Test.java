958b05e19c26fbce60e05e94c66c24903906166e
thrkrif <103479629+thrkrif@users.noreply.github.com>
2022-10-08 15:12:25 +0900

    Java
    study

--- src/Test.java
+++ src/Test.java
@@ -3,12 +3,9 @@ import java.util.Scanner;
 public class Test {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        String str = "3";
 
-        System.out.print("두자리 정수를 하나 입력해주세요.");
-        String input = scanner.nextLine();
-        int num = Integer.parseInt(input);
-        System.out.println("입력 내용: "+input);
-        System.out.printf("num=%d%n", num);
+        System.out.println(str.charAt(0)-'0');
+        System.out.println('3'-'0'+1);
     }
 }
