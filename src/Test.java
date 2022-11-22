a17700b7af1f3676010730806ac4f8620fd4f740
thrkrif <103479629+thrkrif@users.noreply.github.com>
2022-11-22 22:11:12 +0900

    java-study

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,16 @@ import java.util.Scanner;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println(true + null);
+       int i = 5, j = 0;
+
+       j = i++;
+        System.out.println("J = i++; 실행 후, i = " + i +", j = "+ j);
+
+        i = 5;
+        j = 0;
+        
+
+        j =  ++i;
+        System.out.println("j=++i 실행 후, i=" + i +", j="+ j);
     }
 }
