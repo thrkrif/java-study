6333351fa7a1deda17e30b384a764b75ad944e29
thrkrif <103479629+thrkrif@users.noreply.github.com>
2022-12-24 16:08:52 +0900

    java
    study

--- src/Test.java
+++ src/Test.java
@@ -4,10 +4,11 @@ public class Test {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("이름 : ");
-        String name = sc.nextLine();
-        System.out.println(name + "님 안녕하세요");
+        boolean b = true;
+
+        System.out.printf("b = %b%n", b);
+        System.out.printf("!b = %b%n", !b);
+        System.out.println();
 
     }
 }
