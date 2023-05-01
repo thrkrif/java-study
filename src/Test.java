c57c61a016c951357dc3224845d1d086b3c7547b
thrkrif <103479629+thrkrif@users.noreply.github.com>
2023-05-02 00:44:14 +0900

    Java
    Study 이제 다시 시작!

--- src/Test.java
+++ src/Test.java
@@ -4,16 +4,27 @@ public class Test {
 
     public static void main(String[] args) {
 
-        System.out.println("숫자를 하나 입력하세요");
+        int score = 0;
+        char grade = ' ', opt = '0';
+
+        System.out.print("점수를 입력하세요.>");
+
         Scanner scanner = new Scanner(System.in);
-        int input = scanner.nextInt();
+        score = scanner.nextInt();
 
-        if(input == 0){
-            System.out.println("입력하신 숫자는 0입니다.");
-        }
-        else{
-            System.out.println("입력하신 숫자는 0이 아닙니다.");
+        System.out.printf("당신의 점수는 %d입니다.%n", score);
+
+        if (score >= 90){
+            grade = 'A';
+            if(score >= 98){
+                opt = '+';
+            }
+            else{
+                opt = '-';
+            }
         }
 
+
+        System.out.printf("당신의 학점은 %c%c입니다.%n", grade, opt);
     }
 }
