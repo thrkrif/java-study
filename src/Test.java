a4c4f71239eeb190b091c2d2921340521a60d4e2
thrkrif <103479629+thrkrif@users.noreply.github.com>
2023-05-02 20:49:43 +0900

    Java study
    Switch <-> if else

--- src/Test.java
+++ src/Test.java
@@ -10,28 +10,19 @@ public class Test {
         Scanner scanner = new Scanner(System.in);
         int month = scanner.nextInt();
 
-        switch (month){
-            case 3:
-            case 4:
-            case 5:
-                System.out.println("현재의 계절은 봄입니다.");
-                break;
-            case 6:
-            case 7:
-            case 8:
-                System.out.println("현재의 계절은 여름입니다.");
-                break;
-            case 9:
-            case 10:
-            case 11:
-                System.out.println("현재의 계절은 가을입니다.");
-                break;
-            case 12:
-            case 1:
-            case 2:
-                System.out.println("현재의 계절은 겨울입니다.");
-                break;
+        if (month == 3 || month == 4 || month == 5){
+            System.out.println("현재의 계절은 봄입니다.");
         }
+        else if (month == 6 || month == 7 || month == 8){
+            System.out.println("현재의 계절은 여름입니다.");
+        }
+        else if (month == 9 || month == 10 || month == 11){
+            System.out.println("현재의 계절은 가을입니다.");
+        }
+        else {
+            System.out.println("현재의 계절은 겨울입니다.");
+        }
+
 
     }
 }
