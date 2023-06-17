9f659164da68e7271dc8650cbfd9d8e0c9fc526b
thrkrif <103479629+thrkrif@users.noreply.github.com>
2023-06-17 23:54:18 +0900

    Java
    study

--- src/Test.java
+++ src/Test.java
@@ -5,14 +5,14 @@ public class Test {
 
     public static void main(String[] args) {
 
-        int num = 0;
-
         for (int i = 1; i <=5; i++){
-            num = (int)(Math.random() * 6) + 1;
-            System.out.println(num);
+            for(int j = 1; j<=i; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
         }
 
-        
+
 
     }
 }
