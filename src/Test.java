45b6c5ebafff10639fe180f6047a234d9c8e8e08
thrkrif <103479629+thrkrif@users.noreply.github.com>
2023-06-17 23:05:28 +0900

    Java
    study

--- src/Test.java
+++ src/Test.java
@@ -5,24 +5,14 @@ public class Test {
 
     public static void main(String[] args) {
 
-        System.out.println("현재 월을 입력하세요>");
+        int num = 0;
 
-        Scanner scanner = new Scanner(System.in);
-        int month = scanner.nextInt();
-
-        if (month == 3 || month == 4 || month == 5){
-            System.out.println("현재의 계절은 봄입니다.");
-        }
-        else if (month == 6 || month == 7 || month == 8){
-            System.out.println("현재의 계절은 여름입니다.");
-        }
-        else if (month == 9 || month == 10 || month == 11){
-            System.out.println("현재의 계절은 가을입니다.");
-        }
-        else {
-            System.out.println("현재의 계절은 겨울입니다.");
+        for (int i = 1; i <=5; i++){
+            num = (int)(Math.random() * 6) + 1;
+            System.out.println(num);
         }
 
+        
 
     }
 }
