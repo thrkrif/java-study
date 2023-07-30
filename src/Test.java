d0092ce97a458a869d90146e40746b701757d42c
thrkrif <103479629+thrkrif@users.noreply.github.com>
2023-07-30 22:59:57 +0900

    java

--- src/Test.java
+++ src/Test.java
@@ -5,20 +5,9 @@ public class Test {
 
     public static void main(String[] args) {
 
-      // 반복문, break문을 이용한다. sum의 총합이 100 이상일때 i의 값과 sum의 값을 출력하는 알고리즘
-        int sum = 0;
-        int i = 0;
+      
+      }
 
-        while(true){
-            if(sum>100)
-                break;
-
-            sum += ++i;
-
-        }
-        System.out.println("" +
-                "i의 값은" + i);
-        System.out.println("sum의 값은" + sum);
 
        }
 
