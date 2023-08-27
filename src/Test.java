46a99a7bc06919c47d965e6a37c5a0064e5a8912
thrkrif <103479629+thrkrif@users.noreply.github.com>
2023-08-27 18:47:50 +0900

    java
    study

--- src/Test.java
+++ src/Test.java
@@ -5,12 +5,20 @@ public class Test {
 
     public static void main(String[] args) {
 
-      
+
+    int[] elements = {20,30,50,10,40};
+    int max, min = elements[0];
+    for(int i = 1; i < elements.length; i++){
+        if(min > elements[i])
+            min = elements[i];
+
+    }
+        System.out.println(min
+        );
+
       }
 
 
-       }
-
-
-
 }
+
+
