d1928e3d631b630555166c4bb44585498ec2cc3f
thrkrif <103479629+thrkrif@users.noreply.github.com>
2023-12-20 17:04:10 +0900

    java
    study

--- src/Test.java
+++ src/Test.java
@@ -4,19 +4,14 @@ import java.util.Scanner;
 
 class MyMath2 {
 
-    long a, b;
+    int add(int[] a){
+        System.out.print("int add(int[] a) - ");
+        int result = 0;
+        for(int i = 0; i < a.length; i++)
+            result += a[i];
 
-    long add(){return a+b;}
-    long subtract(){return a-b;}
-    long multiply(){return a*b;}
-    double divide(){return a/b;}
-    // 인스턴스 메서드
-
-    // 클래스(static) 메서드
-    static long add(long a, long b){return a+b;}
-    static long subtract(long a, long b){return a-b;}
-    static long multiply(long a, long b){return a*b;}
-    static double divide(double a, double b){return a/b;}
+        return result;
+    }
 
 }
 
@@ -24,13 +19,10 @@ public class Test {
 
     public static void main(String[] args) {
 
-        System.out.println(MyMath2.add(200,100));
-
         MyMath2 mm = new MyMath2();
-        mm.a = 100;
-        mm.b = 200;
-        System.out.println(mm.add());
 
+        int[] a = {100,200,300};
+        System.out.println("mm.add(a) 결과: " + mm.add(a));
     }
 
 }
