f74f0075d86c3e8fb6cbe84e27e2182cdbb480f9
thrkrif <103479629+thrkrif@users.noreply.github.com>
2023-12-20 16:48:51 +0900

    java
    study

--- src/Test.java
+++ src/Test.java
@@ -2,24 +2,35 @@ import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.Scanner;
 
-class Data2 {int x;}
+class MyMath2 {
+
+    long a, b;
+
+    long add(){return a+b;}
+    long subtract(){return a-b;}
+    long multiply(){return a*b;}
+    double divide(){return a/b;}
+    // 인스턴스 메서드
+
+    // 클래스(static) 메서드
+    static long add(long a, long b){return a+b;}
+    static long subtract(long a, long b){return a-b;}
+    static long multiply(long a, long b){return a*b;}
+    static double divide(double a, double b){return a/b;}
+
+}
 
 public class Test {
 
     public static void main(String[] args) {
 
-        Data2 d = new Data2();
-        d.x = 10;
-        System.out.println("main 메서드의 d.x="+ d.x +"입니다.");
+        System.out.println(MyMath2.add(200,100));
 
-        change(d);
-        System.out.println("After change(d)");
-        System.out.println("main 메서드의 d.x="+ d.x +"입니다.");
-    }
+        MyMath2 mm = new MyMath2();
+        mm.a = 100;
+        mm.b = 200;
+        System.out.println(mm.add());
 
-    static void change(Data2 d){
-        d.x=1000;
-        System.out.println("change 메서드의 d.x="+d.x+"입니다.");
     }
 
 }
