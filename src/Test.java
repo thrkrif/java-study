d7bb285075560a25fe18d641d13c4b1561a07740
thrkrif <103479629+thrkrif@users.noreply.github.com>
2024-03-08 12:48:16 +0900

    java
    study

--- src/Test.java
+++ src/Test.java
@@ -2,16 +2,20 @@ import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.Scanner;
 
-package com;
 
-class PackageTest {
+
+public class Test {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        for (int j = 1; j <= 5; j++) {
+            for (int i = 1; i <= j; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
     }
-
 }
 
 
